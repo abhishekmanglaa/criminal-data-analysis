7a21c71565d9ad84ebf8b62230e297b7d09d87a3
Akshansh <akshansh461@gmail.com>
2018-03-05 13:16:48 +0530

    Added

--- CriminalAnalysis/ui.R
+++ CriminalAnalysis/ui.R
@@ -1,6 +1,5 @@
 library(shiny)
 
-<<<<<<< HEAD
 shinyUI
 (
   pageWithSidebar
@@ -18,55 +17,19 @@ shinyUI
       
       wellPanel
       (
-=======
-shinyUI(
-  
-  pageWithSidebar(
-    
-    headerPanel("Chicago crime data visualisation"),
-    
-    sidebarPanel(
-      
-      wellPanel(
-        helpText(HTML("<b>Ready?</b>")),
-        HTML("Scroll down to modify the settings. Click this when you are ready to render new plots."),
-        submitButton("Update graphs and tables")
-        ),
-      
-      wellPanel(
->>>>>>> 32b29880c52eaa4757fe1eee21ff2e8690fe3566
         helpText(HTML("<b>BASIC SETTING</b>")),
-        selectInput("crimetype","Select crime type.",
-                    choices=c(unique(crime.data$crime))),
+        selectInput("crimetype","Select crime type.",choices=c(unique(crime.data$crime))),
         helpText("Examples: BATTERY, THEFT etc."),
-        dateInput("startdate",
-                  "Start Date of Data Collection:",
-                  value = "2000-01-01", 
-                  format = "mm-dd-yyyy",
-                  min = "2000-01-01", 
-                  max = "2014-09-29"),
-        dateInput("enddate", 
-                  "End Date of Data Collection:", 
-                  value = "2015-01-02", 
-                  format = "mm-dd-yyyy",
+        dateInput("startdate", "Start Date of Data Collection:", value = "2000-01-01", format = "mm-dd-yyyy",
+                  min = "2000-01-01", max = "2014-09-29"),
+        dateInput("enddate", "End Date of Data Collection:", value = "2015-01-02", format = "mm-dd-yyyy",
                   min = "startdate", max = "2014-09-30"),
-        
         helpText("MM-DD-YYYY as Date format")
       ),
       
-<<<<<<< HEAD
       wellPanel
       (
         selectInput('community','Community Area', choices = commNames, selected = "Chicago-All",selectize=TRUE),
-=======
-      wellPanel(
-        selectInput('community',
-                    'Community Area',
-                    choices = commNames, 
-                    selected = "Chicago-All",
-                    selectize=TRUE),
-        
->>>>>>> 32b29880c52eaa4757fe1eee21ff2e8690fe3566
         helpText("Applies to Crime Map, Analysis, and Weather sections")
       ),
       
@@ -77,19 +40,11 @@ shinyUI(
       )
     ), 
     
-<<<<<<< HEAD
     mainPanel
     (
       tableOutput("try")
     )
     
-=======
-    
-    
-    mainPanel(
-      
-    )
->>>>>>> 32b29880c52eaa4757fe1eee21ff2e8690fe3566
   )
 )
 
