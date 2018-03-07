cf5dcac68394b93d6746850a38693c0a5a75549e
Akshansh <akshansh461@gmail.com>
2018-03-07 22:56:38 +0530

    added

--- CriminalAnalysis/server.R
+++ CriminalAnalysis/server.R
@@ -6,14 +6,20 @@ shinyServer
 (
   function(input, output) 
   {
-    output$try <- renderTable
-    (
-      {
-        commAreaSubset=subset(commArea,input$community)
-        commNo=commAreaSubset$X1
-        subCrimeData=subset(crime.data,crime.data$`Primary Type`==input$crimetype&&crime.data$`Community Area`==commNo)
-        summary(subCrimeData)
-      }
-    )
+    #output$try <- renderTable
+    #(
+     # {
+      #  commAreaSubset=subset(commArea,input$community)
+       # commNo=commAreaSubset$X1
+        #subCrimeData=subset(crime.data,crime.data$`Primary Type`==input$crimetype&&crime.data$`Community Area`==commNo)
+        #summary(subCrimeData)
+      #}
+    #)
+    output$table=renderTable({
+      head(commNames)
+    })
+    output$summr=renderPrint({
+      summary(commNames)
+    })
   }
 )
--- CriminalAnalysis/ui.R
+++ CriminalAnalysis/ui.R
@@ -2,52 +2,33 @@ library(shiny)
 
 shinyUI
 (
-  pageWithSidebar
+  fluidPage
   (
     headerPanel("Chicago crime data visualisation"),
-    sidebarPanel
+    sidebarLayout
     (
-      
-      wellPanel
+      sidebarPanel
       (
-        helpText(HTML("<b>Ready?</b>")),
-        HTML("Scroll down to modify the settings. Click this when you are ready to render new plots."),
-        submitButton("Update graphs and tables")
+        wellPanel
+        (
+          helpText(HTML("<b>BASIC SETTING</b>")),
+          selectInput("crimetype","Select crime type.",choices=c(unique(crime.data$crime))),
+          helpText("Examples: BATTERY, THEFT etc."),
+          dateInput("startdate", "Start Date of Data Collection:", value = "2000-01-01", format = "mm-dd-yyyy",min = "2000-01-01", max = "2014-09-29"),
+          #dateInput("enddate", "End Date of Data Collection:", value = "2015-01-02", format = "mm-dd-yyyy",min = "startdate", max = "2014-09-30"),
+          helpText("MM-DD-YYYY as Date format")
+        )
       ),
-      
-      wellPanel
+      mainPanel
       (
-        helpText(HTML("<b>BASIC SETTING</b>")),
-        selectInput("crimetype","Select crime type.",choices=c(unique(crime.data$crime))),
-        helpText("Examples: BATTERY, THEFT etc."),
-        dateInput("startdate", "Start Date of Data Collection:", value = "2000-01-01", format = "mm-dd-yyyy",
-                  min = "2000-01-01", max = "2014-09-29"),
-        dateInput("enddate", "End Date of Data Collection:", value = "2015-01-02", format = "mm-dd-yyyy",
-                  min = "startdate", max = "2014-09-30"),
-        helpText("MM-DD-YYYY as Date format")
-      ),
-      
-      wellPanel
-      (
-        selectInput('community','Community Area', choices = commNames, selected = "Chicago-All",selectize=TRUE),
-        helpText("Applies to Crime Map, Analysis, and Weather sections")
-      ),
-      
-      wellPanel
-      (
-        selectInput("period", "Choose period for analysis:", choice = c("Monthly","Weekly","Daily","Yearly")),
-        helpText("Applies to Analysis and Weather sections")
-      )
-    ), 
-    
-    mainPanel
-    (
-      tabsetPanel
-      (
-        tabPanel("Basic Stats",show)
+        tabsetPanel
+        (
+          type="tab",
+          tabPanel("Print",tableOutput("table")),
+          tabPanel("Summary",verbatimTextOutput("summr"))
+        )
       )
     )
-    
   )
 )
 
