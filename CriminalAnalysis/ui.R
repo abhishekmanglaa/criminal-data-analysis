10c18e20709924054c781c4a4cb099d00ca8ca0e
Abhishek Mangla <abhishekmangla@Abhisheks-MacBook-Pro-2.local>
2018-03-07 23:00:34 +0530

    Idk

--- CriminalAnalysis/ui.R
+++ CriminalAnalysis/ui.R
@@ -8,29 +8,6 @@ shinyUI
     sidebarLayout
     (
       sidebarPanel
-<<<<<<< HEAD
-      (
-        wellPanel
-        (
-          helpText(HTML("<b>BASIC SETTING</b>")),
-          selectInput("crimetype","Select crime type.",choices=c(unique(crime.data$crime))),
-          helpText("Examples: BATTERY, THEFT etc."),
-          dateInput("startdate", "Start Date of Data Collection:", value = "2000-01-01", format = "mm-dd-yyyy",min = "2000-01-01", max = "2014-09-29"),
-          #dateInput("enddate", "End Date of Data Collection:", value = "2015-01-02", format = "mm-dd-yyyy",min = "startdate", max = "2014-09-30"),
-          helpText("MM-DD-YYYY as Date format")
-        )
-      ),
-      mainPanel
-      (
-        tabsetPanel
-        (
-          type="tab",
-          tabPanel("Print",tableOutput("table")),
-          tabPanel("Summary",verbatimTextOutput("summr"))
-        )
-      )
-    )
-=======
       (
         wellPanel
         (
@@ -77,9 +54,7 @@ shinyUI
       )
     ) 
     
-    
-    
->>>>>>> b9d3194aa1334399c4a58527b8693a2568cd9d41
+
   )
 )
 
